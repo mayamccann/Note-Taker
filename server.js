1e3499ee8e3da8dcd1f5578a778f403a2f8feea0
Maya McCann <mccannmaya@gmail.com>
2023-08-30 12:33:45 -0400

    routes require

--- server.js
+++ server.js
@@ -9,3 +9,6 @@ app.use(express.static('public'));
 app.use(express.urlencoded ({ extended: true}));
 app.use(express.json));
 
+require('./routes/apiRoutes')(app);
+require('./routes/htmlroutes')(app);
+
