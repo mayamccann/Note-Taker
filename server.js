bb575cff61b6982c2ce8df4019038a6e003ee1ea
Maya McCann <mccannmaya@gmail.com>
2023-11-16 16:40:13 -0500

    app.listen console.log for server.js

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ const express = require('express');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
+
+// An example of route to 404 page
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
