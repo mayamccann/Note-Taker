943709505edcf94d490797768ee0b8d9bde92c56
Maya McCann <mccannmaya@gmail.com>
2023-11-16 23:02:33 -0500

    const express, app, module.exports for noteRoutes.js for routes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,4 +1,13 @@
+const express = require('express');
+
+
+const app = express();
+
+
+
 module.exports = (app) => {
 
 
 };
+
+// repeat structure codes for const express, const app and module.exports
